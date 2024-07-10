3b2753ffbe33a82fc42b0272656f3094d76a5efe
Jesus <113149095+jseusx@users.noreply.github.com>
2024-07-10 12:33:53 -0400

    Added HashMap Reader
    Can be used to deserialize the HashMap and view what is stored inside.

---
+++ csvReader/HashMapReader.java
@@ -0,0 +1,70 @@
+package csvReader;
+
+import java.util.Scanner;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.HashMap;
+
+public class HashMapReader {
+    private static final String HASHMAP_FILE = "C:\\Users\\picka\\Downloads\\ipad CSV\\hashmap.ser";
+	
+	public static void main(String[] args) {
+		HashMap<String, String> tf = null;
+		Scanner keyboard = new Scanner(System.in);
+		
+		tf = deserializeHashMap();
+		
+		System.out.println("The current map looks like: ");
+		System.out.println(tf.keySet());
+		System.out.println(tf.values());
+		
+		boolean flag = true;
+		
+		while(flag)
+		{
+			System.out.println("Enter the key or press enter to exit");
+			String key = keyboard.nextLine();
+			//keyboard.nextLine();
+			
+			if(key.isBlank())
+			{
+				flag = false;
+				continue;
+			}
+			System.out.println("Enter the value associated with the key");
+			String value = keyboard.nextLine();
+			
+			tf.put(key, value);
+		
+		}
+		
+		serializeHashMap(tf);
+		keyboard.close();
+	}
+
+	private static void serializeHashMap(HashMap<String, String> tf) {
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(HASHMAP_FILE))) {
+            oos.writeObject(tf);
+            System.out.println("HashMap serialized");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    private static HashMap<String, String> deserializeHashMap() {
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(HASHMAP_FILE))) {
+            return (HashMap<String, String>) ois.readObject();
+        } catch (FileNotFoundException e) {
+            System.out.println("HashMap file not found, starting with a new one.");
+            return null;
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
